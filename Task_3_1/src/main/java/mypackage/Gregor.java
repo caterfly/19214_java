be9e017923d064358d887998534c6713b7d90713
akyrgys <42936641+akyrgys@users.noreply.github.com>
2020-12-26 13:34:44 +0700

    My calendar

--- Task_3_1/src/main/java/mypackage/Gregor.java
+++ Task_3_1/src/main/java/mypackage/Gregor.java
@@ -1,8 +1,6 @@
 package mypackage;
 
-/**
- * Implementation of gregorian calendar based on MyCalendar interface
- */
+
 public class Gregor {
 
     private int[] monthCodes = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
